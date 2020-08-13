9bafe6569cf38b214b671f5a2788ef866a9abfbe
Tony <tony@email.local>
2020-08-13 10:01:10 -0500

    Assessment completed

---
+++ src/components/imageslot/ImageSlot.js
@@ -0,0 +1,24 @@
+//unused function
+/*
+import React from "react";
+import bunny from "../../assets/images/bunny.png";
+import hound from "../../assets/images/hound.png";
+import raccoon from "../../assets/images/raccoon.png";
+import squirrel from "../../assets/images/squirrel.png";
+
+export default class ImageSlot extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="ImageSlot">
+        <div className="slot">
+          <img src={this.props.image} alt={this.props.alt} />
+        </div>
+      </div>
+    );
+  }
+}
+*/
